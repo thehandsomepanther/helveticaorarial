924af96b9e845eaa00432e447b15e737eafc61b2
Joshua Shi <thehandsomepanther@gmail.com>
2015-02-03 21:44:00 -0600

    Expanded word bank

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,209 @@
 var num_guesses = 0;
 var num_correct = 0;
-var first_words = ["Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Over", "Dog", "Days", "Through", "Grind"];
-var second_words = ["everlasting", "light", "next", "girl", "let", "me", "tree", "wait", "makes", "road"];
+var first_words = ["adorable",
+"beautiful",
+"clean",
+"drab",
+"elegant",
+"fancy",
+"glamorous",
+"handsome",
+"long",
+"magnificent",
+"old-fashioned",
+"plain",
+"quaint",
+"sparkling",
+"ugliest",
+"unsightly",
+"wide-eyed",
+"red",
+"orange",
+"yellow",
+"green",
+"blue",
+"purple",
+"gray",
+"black",
+"white",
+"alive",
+"better",
+"careful",
+"clever",
+"dead",
+"easy",
+"famous",
+"gifted",
+"helpful",
+"important",
+"inexpensive",
+"mushy",
+"odd",
+"powerful",
+"rich",
+"shy",
+"tender",
+"uninterested",
+"vast",
+"wrong",
+"angry",
+"bewildered",
+"clumsy",
+"defeated",
+"embarrassed",
+"fierce",
+"grumpy",
+"helpless",
+"itchy",
+"jealous",
+"lazy",
+"mysterious",
+"nervous",
+"obnoxious",
+"panicky",
+"repulsive",
+"scary",
+"thoughtless",
+"uptight",
+"worried",
+"agreeable",
+"brave",
+"calm",
+"delightful",
+"eager",
+"faithful",
+"gentle",
+"happy",
+"jolly",
+"kind",
+"lively",
+"nice",
+"obedient",
+"proud",
+"relieved",
+"silly",
+"thankful",
+"victorious",
+"witty",
+"zealous",
+"broad",
+"chubby",
+"crooked",
+"curved",
+"deep",
+"flat",
+"high",
+"hollow",
+"low",
+"narrow",
+"round",
+"shallow",
+"skinny",
+"square"];
+var second_words = ["ball",
+"bat",
+"bed",
+"book",
+"boy",
+"bun",
+"can",
+"cake",
+"cap",
+"car",
+"cat",
+"cow",
+"cub",
+"cup",
+"dad",
+"day",
+"dog",
+"doll",
+"dust",
+"fan",
+"feet",
+"girl",
+"gun",
+"hall",
+"hat",
+"hen",
+"jar",
+"kite",
+"man",
+"map",
+"men",
+"mom",
+"pan",
+"pet",
+"pie",
+"pig",
+"pot",
+"rat",
+"son",
+"sun",
+"toe",
+"tub",
+"van",
+"actor",
+"airplane",
+"airport",
+"army",
+"baseball",
+"beef",
+"birthday",
+"boy",
+"brush",
+"bushes",
+"butter",
+"cast",
+"cave",
+"cent",
+"cherries",
+"cherry",
+"cobweb",
+"coil",
+"cracker",
+"dinner",
+"eggnog",
+"elbow",
+"face",
+"fireman",
+"flavor",
+"gate",
+"glove",
+"glue",
+"goldfish",
+"goose",
+"grain",
+"hair",
+"haircut",
+"hobbies",
+"holiday",
+"hot",
+"jellyfish",
+"ladybug",
+"mailbox",
+"number",
+"oatmeal",
+"pail",
+"pancake",
+"pear",
+"pest",
+"popcorn",
+"queen",
+"quicksand",
+"quiet",
+"quilt",
+"rainstorm",
+"scarecrow",
+"scarf",
+"stream",
+"street",
+"sugar",
+"throne"];
 
 function genRandomPhrase() {
-    var first_num = Math.floor(Math.random() * 10);
-    var second_num = Math.floor(Math.random() * 10);
+    var first_num = Math.floor(Math.random() * 100);
+    var second_num = Math.floor(Math.random() * 100);
     return first_words[first_num] + ' ' + second_words[second_num];
 }
 
@@ -38,7 +236,7 @@ function genRandomNumber() {
 function endGame() {
     $('.game').fadeOut();
     $('.endGame-text').html(num_correct + "/" + num_guesses);
-    setTimeout(function(){
+    setTimeout(function () {
         $('.endGame').fadeIn();
     }, 500);
 }
@@ -75,10 +273,10 @@ $(document).ready(function () {
         } else {
             num = genRandomNumber();
             reset(num);
-        } 
+        }
         num_guesses++;
-        
-        if (num_guesses >= 10) { 
+
+        if (num_guesses >= 10) {
             endGame();
         }
     });
@@ -92,14 +290,14 @@ $(document).ready(function () {
             reset(num);
             num_correct++;
         }
-        num_guesses++; 
+        num_guesses++;
         if (num_guesses >= 10) {
             endGame();
         }
     });
-    
-    $('.endGame-button').click(function() {
-        restartGame(); 
+
+    $('.endGame-button').click(function () {
+        restartGame();
     });
 
 });
